2693a83cca566b88c02a8924a5992e9537fb9fd3
clairedeboer <claire.deboer@gmail.com>
2021-02-07 17:12:38 -0500

    Done.

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
+import Stock from "./Stock";
 
-function MainContainer() {
+function MainContainer({ stocks }) {
+  const [portfolioStocks, setPortfolioStocks] = useState([]);
+  const [displayStocks, setDisplayStocks] = useState([]);
+
+  const buyStock = (boughtStock) => {
+    setPortfolioStocks([...portfolioStocks, boughtStock]);
+  };
+
+  const alphaSort = () => {
+    const alphaStocks = [...stocks].sort((a, b) => (a.name > b.name ? 1 : -1));
+    setDisplayStocks(alphaStocks);
+  };
+
+  const priceSort = () => {
+    const priceStocks = [...stocks].sort((a, b) => (a.price > b.price ? 1 : -1));
+    setDisplayStocks(priceStocks);
+  };
+
+  const filter = (chosenType) => {
+    const filteredStocks = stocks.filter((stock) => stock.type === chosenType);
+    setDisplayStocks(filteredStocks);
+    console.log(filteredStocks);
+  };
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar
+        onAlphaSort={alphaSort}
+        onPriceSort={priceSort}
+        onFilter={filter}
+      />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={displayStocks.length > 0 ? displayStocks : stocks} onBuyStock={buyStock} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer
+            portfolioStocks={portfolioStocks}
+            stocks={stocks}
+          />
         </div>
       </div>
     </div>
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-function Stock() {
+function Stock({ name, ticker, price, onBuyStock }) {
+  const handleClick = () => {
+    onBuyStock({name, ticker, price})
+  }
+ 
   return (
     <div>
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+      <div className="card" onClick={handleClick}>
+        <div className="card-body" >
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">{ticker}: {price}</p>
         </div>
       </div>
     </div>
